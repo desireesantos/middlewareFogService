35df9960dcf7127c1a0f50d0ace210553b1975f6
Desiree Santos <desisant@gmail.com>
2021-03-07 14:20:12 -0300

    Create README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# middlewareFogService
+Middleware running at fog level responsible for decide which Iot protocol to apply based on network conditions
--- src/constants/enumsProtocols.js
+++ src/constants/enumsProtocols.js
@@ -1,7 +1,7 @@
 module.exports = protocols = {
-	MQTT: "mqtt",
-	COAP: "coap",
-	AMQP: "amqp",
-	DDS: "dds",
-    HTTP: "http",
-}
+  MQTT: "mqtt",
+  COAP: "coap",
+  AMQP: "amqp",
+  DDS: "dds",
+  HTTP: "http",
+};
